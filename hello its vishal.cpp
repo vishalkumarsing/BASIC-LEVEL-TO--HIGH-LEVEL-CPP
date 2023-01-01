772e9121c57f4d5d44de31fa68c4f544fc3e903b
vishal kumar singh <106993591+vishalkumarsing@users.noreply.github.com>
2023-01-02 00:03:26 +0530

    Create hello its vishal.cpp

---
+++ hello its vishal.cpp
@@ -0,0 +1,11 @@
+//hello its vishal singh//
+
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+    // Write C++ code here
+    std::cout << "its vishal singh";
+
+    return 0;
+}
