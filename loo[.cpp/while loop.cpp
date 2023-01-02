5c85f100e0b188c76a19e862b0fca6a6c9fca339
vishal kumar singh <106993591+vishalkumarsing@users.noreply.github.com>
2023-01-02 23:28:05 +0530

    Create while loop.cpp

---
+++ loo[.cpp/while loop.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+ 
+int main () {
+   
+   int a =0;
+
+   while( a<50 ) {
+      cout << "value of a: " << a << endl;
+      a++;
+   }
+
+   return 0;
+}
